8f16d9eef7a21f1d305d003fbeaf3f14031da475
Pierre Rudloff <contact@rudloff.pro>
2016-07-14 00:53:40 +0200

    Apply changes by Trey (https://www.mediawiki.org/wiki/Extension_talk:MailDigest#Working_in_1.16.4_.2C_with_some_modifications)

--- MailDigest.i18n.php
+++ MailDigest.i18n.php
@@ -4,6 +4,7 @@ $messages = array();
 $messages['en'] = array(
     'tog-emailwatch' => 'Mail my watchlist',
     'tog-emailplainformat' => 'Mail using plain text format',
+    'prefs-maildigest'      => 'Mail Digest Settings',
     'watchdetails' => 'Watch list details',
     'watchmethod-recent' => 'Watch list methode',
     'watchnochange' => 'No watch list changes'
--- MailDigest.php
+++ MailDigest.php
@@ -12,21 +12,29 @@ Docs:
 
 $wgExtensionCredits['validextensionclass'][] = array(
        'name' => 'MailDigest',
-       'author' =>'Marco Wobben', 
-       'url' => 'http://www.mediawiki.org/wiki/User:marco.wobben', 
+       'author' =>'Marco Wobben',
+       'url' => 'http://www.mediawiki.org/wiki/User:marco.wobben',
        'description' => 'This extension allows cron to mail a daily digest of watched pages.'
        );
 
 $dir = dirname(__FILE__) . '/';
-	
+
 $wgExtensionMessagesFiles['MailDigest'] = $dir . 'MailDigest.i18n.php';
 require_once( $dir . "MailDigest.body.php" );
-		
-function add_user_toggles(&$extraToggles) 
-{	
-	$extraToggles[] = 'emailwatch'; // ] = 'Email daily Watchlist changes';
- 	$extraToggles[] = 'emailplainformat';// ] = 'Email using plain text (HTML is default)';
- 
+
+function add_user_toggles($user, &$extraToggles) 
+{
+    $extraToggles['emailwatch'] = array( // ] = 'Email daily Watchlist changes';
+               'type' => 'check',
+               'label-message' => 'tog-emailwatch',
+               'section'       => 'misc/maildigest',
+       );
+        $extraToggles['emailplainformat'] = array( // ] = 'Email daily Watchlist changes';
+               'type' => 'check',
+               'label-message' => 'tog-emailplainformat',
+               'section'       => 'misc/maildigest',
+       );
+
  /*
  * Changes in this file will be lost during software upgrades.
  * You can make your customizations on the wiki.
@@ -46,5 +54,5 @@ function maildigest($action, $article)
 	return true;
 }
 
-$wgHooks['UserToggles'][] = array('add_user_toggles');
+$wgHooks['GetPreferences'][] = 'add_user_toggles';
 $wgHooks['UnknownAction'][] = 'maildigest';
